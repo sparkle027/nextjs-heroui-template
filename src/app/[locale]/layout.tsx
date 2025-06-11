875c63767313fe3f7b01c8504c9bebbf442d6f05
mirai <miraiwhite27@gmail.com>
2025-06-12 00:12:57 +0800

    feat: 添加国际化支持，更新页面和布局，增加多语言功能

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,29 @@
+import { NextIntlClientProvider, hasLocale } from 'next-intl'
+import { notFound } from 'next/navigation'
+import { routing } from '@/i18n/routing'
+import { Providers } from '../providers'
+import '@/styles/globals.scss'
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode
+  params: Promise<{ locale: string }>
+}) {
+  // Ensure that the incoming `locale` is valid
+  const { locale } = await params
+  if (!hasLocale(routing.locales, locale)) {
+    notFound()
+  }
+
+  return (
+    <html lang={locale} className="dark">
+      <body>
+        <NextIntlClientProvider>
+          <Providers>{children}</Providers>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  )
+}
---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image } from '@heroui/react'
+import { useTranslations } from 'next-intl'
+// import { Link } from '@/i18n/navigation'
+
+export default function HomePage() {
+  const t = useTranslations('Landing')
+
+  return (
+    <div className="full-screen-center">
+      <Card className="w-[400px]">
+        <CardHeader className="flex gap-3">
+          <Image
+            alt={t('logoAlt')}
+            height={40}
+            radius="sm"
+            src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+            width={40}
+          />
+          <div className="flex flex-col">
+            <p className="text-md">HeroUI</p>
+            <p className="text-small text-default-500">heroui.com</p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <p>{t('desc')}</p>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <Link isExternal showAnchorIcon href="https://github.com/heroui-inc/heroui">
+            {t('visitSource')}
+          </Link>
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
